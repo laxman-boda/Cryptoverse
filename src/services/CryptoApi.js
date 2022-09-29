57a8a73bb15d0f288dca1ca8d9e34c608fc17bc1
laxman-boda <laxman.boda96@gmail.com>
2022-09-29 19:08:07 +0530

    All the crypto data is fetched

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -12,10 +12,16 @@ export const cryptoApi = createApi({
     reducerPath:'cryptoApi',
     baseQuery:fetchBaseQuery({ baseUrl }),
     endpoints:(builder) => ({
-        getCryptos:builder.query({
+        getCryptos: builder.query({
             query:(count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId,timePeriod }) => createRequest(`/coin/${coinId}/history/${timePeriod}`),
         })
     })
 })
   
-export const { useGetCryptosQuery, } = cryptoApi
+export const { useGetCryptosQuery,useGetCryptoDetailsQuery,useGetCryptoHistoryQuery } = cryptoApi
